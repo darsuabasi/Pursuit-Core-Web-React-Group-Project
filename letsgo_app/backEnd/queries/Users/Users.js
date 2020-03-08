2831ef0a8b29919bdff341f6c416f361fd491166
kongnyc <bbsyoow@gmail.com>
2020-03-08 16:54:32 -0400

    add user serarch by email

--- letsgo_app/backEnd/queries/Users/Users.js
+++ letsgo_app/backEnd/queries/Users/Users.js
@@ -21,13 +21,28 @@ const getSingleUser = async (req, res, next) =>{
         let singleUser = await db.one('SELECT * FROM Users WHERE id= $1', [req.params.id]);
         res.status(200).json({
             status: 'success',
-            message: 'retrieves single user',
+            message: 'retrieves single user by ID',
             payload: singleUser
         })
     }catch(error){
         res.status(400).json({
             status: 'error',
-            message: 'could not get single user',
+            message: 'could not get single user by ID',
+        })
+    }
+}
+const getSingleUserByEmail = async (req, res, next) =>{
+    try{
+        let singleUser = await db.one('SELECT * FROM Users WHERE email= $1', [req.params.email]);
+        res.status(200).json({
+            status: 'success',
+            message: 'retrieves single user by Email',
+            payload: singleUser
+        })
+    }catch(error){
+        res.status(400).json({
+            status: 'error',
+            message: 'could not get single user by Email',
         })
     }
 }
@@ -82,4 +97,4 @@ const deleteSingleUser = async (req, res, next) =>{
         })
     }
 }
-module.exports = {getAllUsers, getSingleUser, getNewUser, deleteSingleUser, updateSingleUser}
+module.exports = {getAllUsers, getSingleUser, getNewUser, deleteSingleUser, updateSingleUser,getSingleUserByEmail}
