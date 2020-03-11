20216d8ffb5c2eda074ed0b481ad0275d073055c
kongnyc <bbsyoow@gmail.com>
2020-03-11 19:20:36 -0400

    update user and hash tag into list state

--- letsgo_app/frontend/src/components/SearchAutoComplete.js
+++ letsgo_app/frontend/src/components/SearchAutoComplete.js
@@ -4,8 +4,7 @@ import React, { useState, useEffect } from 'react'
 import axios from "axios";
 
 const SearchAutoComplete =()=> {
-    const [usernameList, setUser] = useState([])
-    const [hashTagList,setHashTag] = useState([])
+    const [list, setList] = useState([])
     const [suggestion, setSuggest]=useState([])
     const [text, setText]=useState("")
 
@@ -13,12 +12,9 @@ const SearchAutoComplete =()=> {
    const handleChange=(e)=>{
         const value =e.target.value
         let suggestion=[];
-        if(value[0]==="#"){
+        if(value.length>0){
             const regex = new RegExp(`${value}`,`i`);
-            suggestion=hashTagList.sort().filter(v=>regex.test(v));
-        }else {
-            const regex = new RegExp(`${value}`,`i`);
-            suggestion=usernameList.sort().filter(v=>regex.test(v));
+            suggestion=list.sort().filter(v=>regex.test(v));
         }
         setSuggest(suggestion);
         setText(value)
@@ -44,13 +40,11 @@ const SearchAutoComplete =()=> {
 
     const fetchData= async(url,setData)=>{
         let res= await axios.get(url)
-        // debugger
         if(res.data.payload[0].username){
             res.data.payload.map((el)=>{
                 setData(prevState=>[...prevState,el.username.toLowerCase()])
             })
         }else if (res.data.payload[0].array_agg){
-            // debugger
                 res.data.payload.map((el)=>{
                     setData(prevState=>[...prevState,...el.array_agg])
                 })
@@ -58,10 +52,10 @@ const SearchAutoComplete =()=> {
     }
 
     useEffect(()=>{
-        fetchData("http://localhost:3005/users/",setUser)
-        fetchData("http://localhost:3005/hashtags/",setUser)
-
+        fetchData("http://localhost:3005/users/",setList)
+        fetchData("http://localhost:3005/hashtags/",setList)
     }, [])
+
         console.log(text)
         return (
             <div>
